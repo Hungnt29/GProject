82839dc2549693f76c9cc60dec10f5a05632d521
Hungnt <hungntph17579@fpt.edu.vn>
2023-04-11 21:24:34 +0700

    feat: function ChangePassword and EditProfile

---
+++ GProject.WebApplication/GProject.WebApplication/Models/ChangePasswordDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GProject.WebApplication.Models
+{
+    public class ChangePasswordDTO
+    {
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+}
